9fce800a180f1ed14fa26d084a5840d0d1bf1fdc
gonch0 <42312277+gonch0@users.noreply.github.com>
2018-11-25 18:56:03 +0500

    Модуль 3 задание 1

---
+++ js/setup.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getWizardName = function (names, surnames) {
+  var name = names[getRandomInt(0, names.length)] + ' ' + surnames[getRandomInt(0, surnames.length)];
+  return name;
+};
+
+var getWizardColor = function (colors) {
+  var color = colors[getRandomInt(0, colors.length)];
+  return color;
+}
+
+var wizards = [];
+
+for (var i = 0; i < 4; i++) {
+  wizards.push({name: getWizardName(WIZARD_NAMES, WIZARD_SURNAMES), coatColor: getWizardColor(WIZARD_COATS), eyesColor: getWizardColor(WIZARD_EYES)})
+}
+
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+}
+
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
