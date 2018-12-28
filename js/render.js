e4e6aafe55a4096e61a1d93ce26a8426bb3f522b
gonch0 <42312277+gonch0@users.noreply.github.com>
2018-12-28 22:45:22 +0500

    m7 t1

---
+++ js/render.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function () {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template');
+  var renderWizard = function (wizard) {
+    var element = similarWizardTemplate.content.cloneNode(true);
+
+    var wizardElement = element.querySelector('.wizard');
+    element.querySelector('.setup-similar-label').innerText = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+    return element;
+  };
+
+  var similar = document.querySelector('.setup-similar');
+  var similarList = document.querySelector('.setup-similar-list');
+
+
+  window.render = function (data) {
+
+    var takeNumber = data.length > 4 ? 4 : data.length;
+    similarList.innerHTML = '';
+
+    for (var i = 0; i < takeNumber; i++) {
+      similarList.appendChild(renderWizard(data[i]));
+    }
+
+    similar.classList.remove('hidden');
+  };
+
+})();
